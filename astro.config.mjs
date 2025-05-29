fedd2eb620b4477be8b50542e960290a8699ff57
svitalis123 <vitalis.wilson@yahoo.com>
2025-05-29 11:47:24 +0300

    compressed iamge

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import compress from 'astro-compress';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cedersportsacademy.com',
-  integrations: [sitemap()],
+  integrations: [sitemap(), compress()],
 });
